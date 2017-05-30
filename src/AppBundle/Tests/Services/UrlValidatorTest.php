db8bbf1d1e774a7df948d7b6ced4d6c31d6dba42
Vladislav Mosnak <vladislav@degree361.com>
2017-05-30 11:50:43 +0200

    add test for urlValidator

---
+++ src/AppBundle/Tests/Services/UrlValidatorTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Test\AppBundle\Services;
+
+use AppBundle\Services\UrlValidator;
+use PHPUnit\Framework\TestCase;
+
+class UrlValidatorTest extends TestCase
+{
+    public function testisUrlValid(){
+        $urlValidator = new UrlValidator();
+
+        $isValid = $urlValidator->isUrlValid('http://google.rs');
+        $this->assertTrue($isValid);
+
+        $isValid = $urlValidator->isUrlValid('google.rs');
+        $this->assertTrue($isValid);
+
+        $isValid = $urlValidator->isUrlValid('www.google.rs');
+        $this->assertTrue($isValid);
+
+        $isValid = $urlValidator->isUrlValid('http://notvalidurl_1123.rs');
+        $this->assertFalse($isValid);
+    }
+}
